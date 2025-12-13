e20e1f3ba617e0f66ff562f5f6c9451556d0d7a6
Davidak <davidgames.youtub@gmail.com>
2025-12-13 18:32:26 +0100

    Reading disk

---
+++ src/bootloader/boot.asm
@@ -0,0 +1,204 @@
+org 0x7C00
+bits 16
+
+%define ENDL 0x0D, 0x0A
+
+; FAT12 header
+jmp short start
+nop
+
+bdb_oem: db 'MSWIN4.1' ; 8 bytes
+bdb_bytes_per_sector: dw 512
+bdb_sectors_per_cluster: db 1
+bdb_reserved_sectors: dw 1
+bdb_fat_count: db 2
+bdb_dir_entries_count: dw 0E0h
+bdb_total_sectors: dw 2880 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type: db 0F0h ; F0 = 3.5" floppy disk
+bdb_sectors_per_fat: dw 9
+bdb_sectors_per_track: dw 18
+bdb_heads: dw 2
+bdb_hidden_sectors: dd 0
+bdb_large_sector_count: dd 0
+
+; extended boot record
+ebr_drive_number: db 0, 0 ; type (0x00 floppy, 0x80 hdd, useless), reserved
+ebr_signature: db 29h
+ebr_volume_id: db 12h, 34h, 56h, 78h ; serial number, value doesn't matter
+ebr_volume_label: db 'DAVID OS   ' ; 11 bytes, padded with spaces
+ebr_system_id: db 'FAT12   ' ; 8 bytes
+
+
+start:
+    jmp main
+
+
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb ; loads next character in al and increments si
+    or al, al ; verify if next character is null
+    jz .done ; jump if zero
+
+    ; call bios interrupt that draws character from al
+    mov ah, 0x0e
+    mov bh, 0 ; page number
+    int 0x10 ; interrrupt (not integer)
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
+
+main:
+    ; setup data segments
+    mov ax, 0 ; can't write to ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00 ; stack grows downwards, we do not want it to override our code
+
+    ; read something from floppy disk
+    ; BIOS should set dl to drive number
+    mov [ebr_drive_number], dl
+    mov ax, 1 ; LBA=1, second sector from disk
+    mov cl, 1 ; read 1 sector
+    mov bx, 0x7E00 ; data should be after the bootloader
+    call disk_read
+
+    ; print message
+    mov si, msg_hello_world
+    call puts
+
+    cli
+    hlt
+
+
+; Error Handlers
+
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h ; wait for keypress
+    jmp 0FFFFH:0 ; jump to beginning of BIOS, should reboot
+
+.halt:
+    cli ; disable interrupts
+    jmp .halt
+
+
+; Disk routines
+
+
+; Converts an LBA adress to CHS adress
+; Parameters:
+;   - ax: LBA adress
+; Returns:
+;   - cx [bits 0-5]: sector number
+;   - cx [bits 6-15]: cylinder
+;   - dh: head
+lba_to_chs:
+    push ax
+    push dx
+
+    xor dx, dx; dx = 0
+    div word [bdb_sectors_per_track] ; ax = LBA / SectorsPerTrack ; dx = LBA % SectorsPerTrack
+
+    inc dx ; dx = (LBA % SectorsPerTrack + 1) = sector
+    mov cx, dx ; cx = sector
+
+    xor dx, dx ; dx = 0
+    div word [bdb_heads] ; ax = (LBA / SectorsPerTrack) / Heads ; dx = (LBA / SectorsPerTrack) % Heads
+
+    mov dh, dl ; dh = head
+    mov ch, al ; ch = cylinder (lower 8 bits)
+    shl ah, 6
+    or cl, ah ; put upper 2 bits of cylinder
+
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+
+
+; Reads sectors from a disk
+; Parameters:
+;   - ax: LBA adress
+;   - cl: number of sectors to read (up to 128)
+;   - dl: drive number
+;   - es:bx: memory adress where to store read data
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx ; temporarily save cl
+    call lba_to_chs ; cx [bits 0-5]: sector number ; cx [bits 6-15]: cylinder
+    pop ax
+
+    mov ah, 02h
+    mov di, 3 ; retry count
+
+.retry
+    pusha ; save all registers, we don't know what BIOS modifies
+    stc ; set carry flag, some BIOS'es don't set it
+    int 13h ; interrupt 13,2 (ah = 02h) - read disk sectors ; carry flag cleared = success
+    jnc .done ; jmp if carry no set (success)
+
+    ; read failed
+    popa ; pop all
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail
+    ; all attempts are exhausted
+    jmp floppy_error
+
+.done
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+
+
+; Reset disk controller
+; Parameters:
+;   - dl: drive number
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+
+
+msg_hello_world: db 'Hello world!', ENDL, 0 ; 0 = null terminator
+msg_read_failed: db 'Read from disk failed!', ENDL, 0
+
+
+times 510-($-$$) db 0
+dw 0AA55h

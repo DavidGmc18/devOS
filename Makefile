9cbba849281598cb600bcbb35b3126e3824b2559
Davidak <davidgames.youtub@gmail.com>
2025-12-22 22:37:50 +0100

    broken

--- Makefile
+++ Makefile
@@ -1,156 +1,70 @@
-ASM = nasm
-CC = gcc
+ASM=nasm
+CC=gcc
 CC16 = /mnt/c/WATCOM/binl/wcc
 LD16 = /mnt/c/WATCOM/binl/wlink
 
-MTOOLS = mcopy -i $(DISK_IMG)
-
-SRC_DIR = src
-BUILD_DIR = build
-
-DISK_IMG = $(BUILD_DIR)/main_disk.img
-IMG_SECTORS = 32768
-RESERVED_SECTORS = 2
+SRC_DIR=src
+BUILD_DIR=build
 
 .PHONY: all disk_image kernel bootloader clean always run
 
 all: disk_image
 
-
+#
 # Disk image
-disk_image: $(DISK_IMG)
-$(DISK_IMG): bootloader kernel
-	dd if=/dev/zero of=$(DISK_IMG) bs=512 count=$(IMG_SECTORS) status=none
-	mkfs.fat -F 16 -f 2 -R $(RESERVED_SECTORS) -s 1 -r 256 -S 512 -n "$(IMG_NAME)" $(DISK_IMG)
+#
+disk_image: $(BUILD_DIR)/main_disk.img
 
-	dd if=$(BUILD_DIR)/stage1.bin of=$(DISK_IMG) bs=1 count=3 conv=notrunc status=none
-	dd if=$(BUILD_DIR)/stage1.bin of=$(DISK_IMG) bs=1 skip=62 seek=62 count=450 conv=notrunc status=none
-# 	dd if=$(BUILD_DIR)/stage2.bin of=$(DISK_IMG) bs=512 seek=1 conv=notrunc status=none
-# 	$(MTOOLS) $(BUILD_DIR)/kernel.bin "::kernel.bin"
-	dd if=$(BUILD_DIR)/kernel.bin of=$(DISK_IMG) bs=512 seek=1 conv=notrunc status=none
+$(BUILD_DIR)/main_disk.img: bootloader kernel
+	dd if=/dev/zero of=$(BUILD_DIR)/main_disk.img bs=512 count=32768
+	mkfs.fat -F 16 -n "NBOS" -s 1 $(BUILD_DIR)/main_disk.img
 
+	dd if=$(BUILD_DIR)/stage1.bin of=$(BUILD_DIR)/main_disk.img bs=1 count=3 conv=notrunc status=none
+	dd if=$(BUILD_DIR)/stage1.bin of=$(BUILD_DIR)/main_disk.img bs=1 skip=62 seek=62 count=450 conv=notrunc status=none
 
+	mcopy -i $(BUILD_DIR)/main_disk.img $(BUILD_DIR)/stage2.bin "::stage2.bin"
+	mcopy -i $(BUILD_DIR)/main_disk.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
+
+#
 # Bootloader
+#
 bootloader: stage1 stage2
 
 stage1: $(BUILD_DIR)/stage1.bin
+
 $(BUILD_DIR)/stage1.bin: always
 	$(MAKE) -C $(SRC_DIR)/bootloader/stage1 BUILD_DIR=$(abspath $(BUILD_DIR))
 
 stage2: $(BUILD_DIR)/stage2.bin
+
 $(BUILD_DIR)/stage2.bin: always
 	$(MAKE) -C $(SRC_DIR)/bootloader/stage2 BUILD_DIR=$(abspath $(BUILD_DIR))
 
-
+#
 # Kernel
+#
 kernel: $(BUILD_DIR)/kernel.bin
+
 $(BUILD_DIR)/kernel.bin: always
 	$(MAKE) -C $(SRC_DIR)/kernel BUILD_DIR=$(abspath $(BUILD_DIR))
 
-
+#
 # Always
+#
 always:
 	mkdir -p $(BUILD_DIR)
 
-
+#
 # Clean
+#
 clean:
 	$(MAKE) -C $(SRC_DIR)/bootloader/stage1 BUILD_DIR=$(abspath $(BUILD_DIR)) clean
 	$(MAKE) -C $(SRC_DIR)/bootloader/stage2 BUILD_DIR=$(abspath $(BUILD_DIR)) clean
 	$(MAKE) -C $(SRC_DIR)/kernel BUILD_DIR=$(abspath $(BUILD_DIR)) clean
 	rm -rf $(BUILD_DIR)/*
 
+#
 # Run
-run: disk_image
-	@echo "Running disk image in QEMU (boot from HD)..."
-	qemu-system-i386 \
-		-drive format=raw,file=$(DISK_IMG),index=0,media=disk \
-		-boot c \
-		-m 64M \
-		-monitor stdio
-
-
-# BOOTLOADER_S0_SRC 	:= $(SRC_DIR)/bootloader/stage0/bootloader.asm
-# BOOTLOADER_S0_BIN 	:= $(BUILD_DIR)/bootloader_s0.bin
-# BOOTLOADER_S1_SRC 	:= $(SRC_DIR)/bootloader/stage1/bootloader.asm
-# BOOTLOADER_S1_BIN 	:= $(BUILD_DIR)/bootloader_s1.bin
-# KERNEL_SRC     		:= $(SRC_DIR)/kernel/main.asm
-# KERNEL_BIN     		:= $(BUILD_DIR)/kernel.bin
-# DISK_IMG       		:= $(BUILD_DIR)/main_disk.img
-
-# # MTools
-# MTOOLS := mcopy -i $(DISK_IMG)
-
-# # Disk image parameters
-# IMG_SECTORS 		:= 32768  # 16MB
-# IMG_NAME    		:= NBOS
-# RESERVED_SECTORS 	:= 2
-
-# # -----------------------------------------------------
-# .PHONY: all disk_image bootloader kernel always clean run
-
-# all: disk_image
-
-# # -----------------------------------------------------
-# # Disk image
-# disk_image: $(DISK_IMG)
-
-# $(DISK_IMG): bootloader kernel
-# 	@echo "Creating empty FAT16 hard disk image..."
-# 	mkdir -p $(BUILD_DIR)
-# 	dd if=/dev/zero of=$(DISK_IMG) bs=512 count=$(IMG_SECTORS) status=none
-# 	mkfs.fat -F 16 -f 2 -R $(RESERVED_SECTORS) -s 1 -r 256 -S 512 -n "$(IMG_NAME)" $(DISK_IMG)
-
-# 	@echo "Copying bootloader..."
-# 	dd if=$(BOOTLOADER_S0_BIN) of=$(DISK_IMG) bs=1 count=3 conv=notrunc status=none
-# 	dd if=$(BOOTLOADER_S0_BIN) of=$(DISK_IMG) bs=1 skip=62 seek=62 count=450 conv=notrunc status=none
-# 	dd if=$(BOOTLOADER_S1_BIN) of=$(DISK_IMG) bs=512 seek=1 conv=notrunc status=none
-
-# 	@echo "Copying kernel..."
-# 	$(MTOOLS) $(KERNEL_BIN) "::kernel.bin"
-
-# # -----------------------------------------------------
-# # Bootloader
-# bootloader: $(BOOTLOADER_S0_BIN) $(BOOTLOADER_S1_BIN)
-
-# $(BOOTLOADER_S0_BIN): always
-# 	@echo "Assembling stage0 bootloader..."
-# 	$(ASM) $(BOOTLOADER_S0_SRC) -f bin -o $(BOOTLOADER_S0_BIN)
-
-# $(BOOTLOADER_S1_BIN): always
-# 	@echo "Assembling stage1 bootloader..."
-# 	$(ASM) $(BOOTLOADER_S1_SRC) -f bin -o $(BOOTLOADER_S1_BIN)
-
-# # -----------------------------------------------------
-# # Kernel
-# kernel: $(KERNEL_BIN)
-
-# $(KERNEL_BIN): always
-# 	@echo "Assembling kernel..."
-# 	$(ASM) $(KERNEL_SRC) -f bin -o $(KERNEL_BIN)
-
-# # -----------------------------------------------------
-# # Ensure build directories exist
-# always:
-# 	mkdir -p $(BUILD_DIR)
-
-# # -----------------------------------------------------
-# # Clean
-# clean:
-# 	@echo "Cleaning build directory..."
-# 	@if [ -d "$(BUILD_DIR)" ]; then \
-# 		rm -rf $(BUILD_DIR)/*; \
-# 		echo "Build files removed."; \
-# 	else \
-# 		echo "Nothing to clean."; \
-# 	fi
-
-# # -----------------------------------------------------
-# # Run in QEMU (boot from HD)
-# run: disk_image
-# 	@echo "Running disk image in QEMU (boot from HD)..."
-# 	qemu-system-i386 \
-# 		-drive format=raw,file=$(DISK_IMG),index=0,media=disk \
-# 		-boot c \
-# 		-m 64M \
-# 		-monitor stdio
+#
+run: $(BUILD_DIR)/main_disk.img
+	qemu-system-i386 -drive file=$(BUILD_DIR)/main_disk.img
--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -3,216 +3,350 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
-start:
-    ; 1. FORCE CS:IP to 0x0000:0x7C00
-    ; Some BIOSes load to 0x07C0:0x0000. This fixes that.
-    jmp 0x0000:entry
+jmp short start
+nop
 
-entry:
-    ; 2. Setup Segments (DS, ES, SS) to 0
-    xor ax, ax
+bdb_oem:                    db '        ' ; 8 bytes
+bdb_bytes_per_sector:       dw 0
+bdb_sectors_per_cluster:    db 0
+bdb_reserved_sectors:       dw 0
+bdb_fat_count:              db 0
+bdb_dir_entries_count:      dw 0
+bdb_total_sectors:          dw 0
+bdb_media_descriptor_type:  db 0
+bdb_sectors_per_fat:        dw 0
+bdb_sectors_per_track:      dw 0
+bdb_heads:                  dw 0
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+; extended boot record
+ebr_drive_number:           db 0
+ebr_reserved:               db 0
+ebr_signature:              db 0
+ebr_volume_id:              dd 0
+ebr_volume_label:           db '           ' ; 11 bytes, padded with spaces
+ebr_system_id:              db '        ' ; 8 bytes
+
+
+start:
+    mov ax, 0           ; can't set ds/es directly
     mov ds, ax
     mov es, ax
+
+    ; setup stack
     mov ss, ax
     mov sp, 0x7C00
 
-    ; 3. Save Boot Drive (Passed by BIOS in DL)
-    mov [boot_drive], dl
+.after:
+    mov [ebr_drive_number], dl
 
-    ; 4. Feedback
-    mov si, msg_load
-    call puts
+    ; read drive parameters (sectors per track and head count),
+    ; instead of relying on data on formatted disk
+    push es
+    mov ah, 08h
+    int 13h
+    jc disk_error
+    pop es
 
-    ; 5. SETUP DISK PACKET (DAP)
-    ; We write directly to the structure to be safe
-    mov si, dap
-    mov word [si], 0x0010   ; Size of packet (16 bytes)
-    mov word [si+2], 1      ; Number of sectors to read
-    mov word [si+4], 0x7E00 ; Offset (Buffer)
-    mov word [si+6], 0x0000 ; Segment (Buffer)
-    mov dword [si+8], 1     ; LBA Low (Sector 1)
-    mov dword [si+12], 0    ; LBA High
+    and cl, 0x3F                        ; remove top 2 bits
+    xor ch, ch
+    mov [bdb_sectors_per_track], cx
 
-    ; 6. READ DISK (INT 13h, AH=42h)
-    mov ah, 0x42
-    mov dl, [boot_drive]
-    int 0x13
-    jc disk_error           ; Jump if Carry Flag is set
+    inc dh
+    mov [bdb_heads], dh
 
-    ; 7. JUMP TO KERNEL
-    ; Far jump to ensuring we are at 0000:7E00
-    mov si, msg_succ
-    call puts
+    ; compute LBA of root directory = reserved + fats * sectors_per_fat
+    mov ax, [bdb_sectors_per_fat]
+    mov bl, [bdb_fat_count]
+    xor bh, bh
+    mul bx                              ; ax = (fats * sectors_per_fat)
+    add ax, [bdb_reserved_sectors]      ; ax = LBA of root directory
+    mov [root_dir_lba], ax
+
+    ; compute size of root directory = (32 * number_of_entries) / bytes_per_sector
+    mov ax, [bdb_dir_entries_count]
+    shl ax, 5                           ; ax *= 32
+    xor dx, dx                          ; dx = 0
+    div word [bdb_bytes_per_sector]     ; number of sectors we need to read
+
+    test dx, dx                         ; if dx != 0, add 1
+    jz .root_dir_after
+    inc ax
+
+.root_dir_after:
+    mov [root_dir_sectors], ax
+
+    ; read root directory
+    mov cx, ax                          ; cx = number of sectors to read = size of root directory
+    mov ax, [root_dir_lba]                              ; ax = LBA of root directory
+    mov bx, buffer                      ; es:bx = buffer
+    call disk_read
+
+    ; search for stage2.bin
+    xor bx, bx
+    mov di, buffer
+
+.search_stage2:
+    mov si, file_stage2_bin
+    mov cx, 11
+    push di
+    repe cmpsb
+    pop di
+    je .found_kernel
+
+    add di, 32
+    inc bx
+    cmp bx, [bdb_dir_entries_count]
+    jl .search_stage2
+
+    jmp stage2_not_found_error
+
+.found_kernel:
+    mov ax, [di + 26]
+    mov [stage2_cluster], ax
+
+    mov ax, 18
+    call print_word_hex
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;     mov ax, [bdb_reserved_sectors]
+;     mov bx, buffer
+;     mov cl, [bdb_sectors_per_fat]
+;     call disk_read
+
+;     mov bx, STAGE2_LOAD_SEGMENT
+;     mov es, bx
+;     mov bx, STAGE2_LOAD_OFFSET
+
+; .load_stage2_loop:
+;     ; Compute LBA
+;     mov ax, [stage2_cluster]
+
+;     sub ax, 2
+;     mov cx, [bdb_sectors_per_cluster]
+;     mul cx
+;     add ax, [root_dir_lba]
+;     add ax, [root_dir_sectors]
     
-    jmp 0x0000:0x7E00
+;     mov cx, [bdb_sectors_per_cluster]
+;     call disk_read
+    
+;     mov ax, [bdb_bytes_per_sector]
+;     mov cx, [bdb_sectors_per_cluster]
+;     mul cx
+;     add bx, ax
 
-; --- ERROR HANDLING ---
-disk_error:
-    mov si, msg_err
-    call puts
-    cli
-    hlt
+;     mov ax, [stage2_cluster]
+;     mov cx, 2
+;     mul cx
 
-; --- HELPERS ---
-puts:
-    push ax
-    push si
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0E
-    int 0x10
-    jmp .loop
-.done:
-    pop si
-    pop ax
-    ret
+;     mov si, buffer
+;     add si, ax
+;     mov ax, [ds:si]
 
-; --- DATA ---
-boot_drive: db 0
-dap: times 16 db 0
+;     cmp ax, 0xFFF8
+;     jae .read_finish
 
-msg_load: db 'Loading...', ENDL, 0
-msg_succ: db 'Jump!', ENDL, 0
-msg_err:  db 'Disk Error!', ENDL, 0
+;     mov [stage2_cluster], ax
+;     jmp .load_stage2_loop
 
-times 510-($-$$) db 0
-dw 0xAA55
+; .read_finish:
+;     mov dl, [ebr_drive_number]
 
-
-
-; %include "../bpb.inc"
-; org 0x7C00
-; bits 16
-
-; %define ENDL 0x0D, 0x0A
-
-; jmp short start
-; nop
-
-; times BDB_SIZE + EBR_SIZE db 0
-
-
-; dap:                    
-;     db 16        ; size of DAP structure in bytes
-;     db 0         ; reserved
-;     dw 0         ; number of sectors to read (fill before INT 13h)
-;     dw 0         ; offset (BX) to load
-;     dw 0         ; segment (ES) to load
-;     dd 0         ; starting LBA (low DWORD)
-;     dd 0         ; starting LBA (high DWORD)
-
-
-; load_boot_sectors: dw 1 ; number of reserved boot sectors, that needs to be loaded (we don't need to load first one as it it handled by BIOS)
-
-
-; start:
-;     xor ax, ax
+;     mov ax, STAGE2_LOAD_SEGMENT         ; set segment registers
 ;     mov ds, ax
 ;     mov es, ax
-;     mov ss, ax
-;     mov sp, 0x7C00
 
-;     mov si, msg_loading
-;     call puts
+;     jmp STAGE2_LOAD_SEGMENT:STAGE2_LOAD_OFFSET
 
-;     mov [EBR_DRIVE_NUMBER], dl
+;     jmp wait_key_and_reboot
 
-;     mov ax, 1
-;     mov cx, [load_boot_sectors]
-;     mov bx, 0x7E00
-;     call disk_read
-;     jmp 0x7E00
+.halt:
+    cli ; disable interrupts, this way CPU can't get out of "halt" state
+    hlt
 
 
-; ; Prints a string to the screen
-; ; Params:
-; ;   - ds:si points to string
-; puts:
-;     push si
-;     push ax
-;     push bx
+disk_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
 
-; .loop:
-;     lodsb               ; loads next character in al
-;     or al, al           ; verify if next character is null?
-;     jz .done
+stage2_not_found_error:
+    mov si, msg_stage2_not_found
+    call puts
+    jmp wait_key_and_reboot
 
-;     mov ah, 0x0E        ; call bios interrupt
-;     mov bh, 0           ; set page number to 0
-;     int 0x10
-
-;     jmp .loop
-
-; .done:
-;     pop bx
-;     pop ax
-;     pop si    
-;     ret
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h                     ; wait for keypress
+    jmp 0FFFFh:0
 
 
-; ; Read from disk
-; ; Params:
-; ;   - ax: LBA
-; ;   - cx: num sectors to read 
-; ;   - bx:es: memory address where to store read data
-; disk_read:
-;     pusha
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
 
-;     mov word [dap + 2], cx       ; sectors to read
-;     mov word [dap + 4], bx       ; offset
-;     mov word [dap + 8], ax       ; LBA low 16 bits
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
 
-;     mov ah, 42h
-;     mov si, dap
-;     mov dl, [EBR_DRIVE_NUMBER]
-;     mov di, 3                    ; retry count
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
 
-; .retry:
-;     stc
-;     int 13h
-;     jnc .done
+    jmp .loop
 
-;     call disk_reset
-;     dec di
-;     test di, di
-;     jnz .retry
-;     jmp disk_error
-
-; .done:
-;     popa
-;     ret
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
 
 
-; ; Resets disk controller
-; disk_reset:
-;     pusha
-;     mov dl, [EBR_DRIVE_NUMBER]
-;     mov ah, 0
-;     stc
-;     int 13h
-;     jc disk_error
-;     popa
-;     ret
+; input: lower 4 bits in AL
+; output: ASCII character in AL
+nibble_to_hex:
+    cmp al, 10
+    jb .digit
+    add al, 'A' - 10
+    ret
+.digit:
+    add al, '0'
+    ret
 
 
-; disk_error:
-;     mov si, msg_read_failed 
-;     call puts
-;     jmp halt
+; input: AL = byte to print
+; uses: AH, AL
+print_byte_hex:
+    push ax
+    mov ah, al
+    shr ah, 4           ; high nibble
+    call nibble_to_hex
+    mov ah, 0x0E
+    int 10h             ; print high nibble
+    pop ax
+    and al, 0x0F        ; low nibble
+    call nibble_to_hex
+    mov ah, 0x0E
+    int 10h             ; print low nibble
+    ret
 
 
+; input: AX = word to print
+; prints high byte then low byte
+print_word_hex:
+    push ax
+    mov al, ah
+    call print_byte_hex
+    pop ax
+    mov al, al
+    call print_byte_hex
+    ret
 
-; halt:
-;     cli
-;     hlt
-;     jmp halt
+
+; Read from disk
+; Params:
+;   - ax: LBA
+;   - cx: num sectors to read 
+;   - es:bx: memory address where to store read data
+disk_read:
+    pusha
+
+    mov word [dap + 2], cx      ; sectors to read
+    mov word [dap + 4], bx      ; offset
+    mov word [dap + 6], es      ; segment    
+    mov word [dap + 8], ax      ; LBA low 16 bits
+
+    mov ah, 42h
+    mov si, dap
+    mov dl, [ebr_drive_number]
+    mov di, 3                    ; retry count
+
+.retry:
+    stc
+    int 13h
+
+    cmp ah, 0
+    jc .fail
+
+    cmp ah, 0
+    jne .fail
+
+    ; cmp al, cl
+    ; jne .fail
+
+    jmp .done
+
+.fail:
+    call disk_reset
+    dec di
+    test di, di
+    jnz .retry
+    jmp disk_error
+
+.done:
+    popa
+    ret
 
 
-; msg_loading:        db 'Loading...', ENDL, 0
-; msg_read_failed:        db 'Read from disk failed!', ENDL, 0
+; Resets disk controller
+disk_reset:
+    pusha
+    mov dl, [ebr_drive_number]
+    mov ah, 0
+    stc
+    int 13h
+    jc disk_error
+    popa
+    ret
 
 
-; times 510-($-$$) db 0
-; dw 0xAA55
+dap:                    
+    db 16        ; size of DAP structure in bytes
+    db 0         ; reserved
+    dw 0         ; number of sectors to read (fill before INT 13h)
+    dw 0         ; offset (BX) to load
+    dw 0         ; segment (ES) to load
+    dd 0         ; starting LBA (low DWORD)
+    dd 0         ; starting LBA (high DWORD)
+
+
+msg_read_failed:        db 'Read from disk failed!', ENDL, 0
+msg_stage2_not_found:   db 'STAGE2.BIN file not found!', ENDL, 0
+file_stage2_bin:        db 'KERNEL  BIN'
+
+; msg_true:               db 'T', 0
+; msg_false:               db 'F', 0
+    ; cmp ax, 1128
+    ; je .true
+    ; mov si, msg_false
+    ; call puts
+    ; jmp .end
+    ; .true:
+    ; mov si, msg_true
+    ; call puts
+    ; .end:
+    ; mov ax, 0x3
+    ; mov cx, 1
+
+root_dir_lba:           dw 0
+root_dir_sectors:       dw 0
+stage2_cluster:         dw 0
+
+STAGE2_LOAD_SEGMENT     equ 0x2000
+STAGE2_LOAD_OFFSET      equ 0
+
+
+times 510-($-$$) db 0
+dw 0AA55h
+
+buffer:

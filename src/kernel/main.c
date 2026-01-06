a491b5d545695913c29a14b67707e31f522de76a
Davidak <davidgames.youtub@gmail.com>
2026-01-06 17:36:49 +0100

    HAL

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,11 +1,14 @@
 #include <stdint.h>
 #include "stdio.h"
 #include "memory.h"
+#include <hal/hal.h>
 
 extern uint8_t __bss_start;
 extern uint8_t __end;
 
 void __attribute__((section(".entry"))) start(uint16_t bootDrive) {
+    HAL_Initialize();
+
     memset(&__bss_start, 0, (&__end) - (&__bss_start));
 
     clrscr();

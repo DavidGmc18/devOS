146acd6f840024a9ac145c7255d776f6b678ab27
Davidak <davidgames.youtub@gmail.com>
2026-01-07 12:43:29 +0100

    Ineterrupts

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -7,13 +7,17 @@ extern uint8_t __bss_start;
 extern uint8_t __end;
 
 void __attribute__((section(".entry"))) start(uint16_t bootDrive) {
-    HAL_Initialize();
-
     memset(&__bss_start, 0, (&__end) - (&__bss_start));
+
+    HAL_Initialize();
 
     clrscr();
 
     printf("Hello world from kernel!!!\n");
+
+    __asm("int $0x2");
+    __asm("int $0x3");
+    __asm("int $0x4");
 
 end:
     for (;;);

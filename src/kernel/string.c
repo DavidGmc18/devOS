5ae2773f48e75b5acfb115a2c287f2f31b73dd98
Davidak <davidgames.youtub@gmail.com>
2026-01-17 11:44:15 +0100

    strncpy

--- src/kernel/string.c
+++ src/kernel/string.c
@@ -12,3 +12,28 @@ size_t strlen(const char* str) {
 
     return len;
 }
+
+char* strncpy(char *dst, const char *src, size_t count) {
+    char* origDst = dst;
+
+    if (dst == NULL) {
+        return NULL;
+    }
+
+    if (src == NULL) {
+        *dst = '\0';
+        return dst;
+    }
+
+    while (*src) {
+        *dst = *src;
+        src++;
+        dst++;
+        if (--count <= 0) {
+            return origDst;
+        }
+    }
+    
+    *dst = '\0';
+    return origDst;
+}
--- src/kernel/string.h
+++ src/kernel/string.h
@@ -3,3 +3,5 @@
 #include <stddef.h>
 
 size_t strlen(const char* str);
+
+char* strncpy(char *dst, const char *src, size_t count);

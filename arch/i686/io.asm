53f1f43a17ae9328a96e7213c0d6256c91559357
Davidak <davidgames.youtub@gmail.com>
2026-02-02 21:07:32 +0100

    IO

--- arch/i686/io.asm
+++ arch/i686/io.asm
@@ -1,19 +1,3 @@
-global i686_outb
-i686_outb:
-    [bits 32]
-    mov dx, [esp + 4]
-    mov al, [esp + 8]
-    out dx, al
-    ret
-
-global i686_inb
-i686_inb:
-    [bits 32]
-    mov dx, [esp + 4]
-    xor eax, eax
-    in al, dx
-    ret
-
 global i686_EnableInterrupts
 i686_EnableInterrupts:
     sti
--- arch/i686/io.h
+++ arch/i686/io.h
@@ -2,8 +2,14 @@
 
 #include <stdint.h>
 
-void __attribute__((cdecl)) i686_outb(uint16_t port, uint8_t value);
-uint8_t __attribute__((cdecl)) i686_inb(uint16_t port);
+void i686_outb(uint16_t port, uint8_t value);
+void i686_outw(uint16_t port, uint16_t value);
+void i686_outl(uint16_t port, uint32_t value);
+
+uint8_t i686_inb(uint16_t port);
+uint16_t i686_inw(uint16_t port);
+uint32_t i686_inl(uint16_t port);
+
 uint8_t __attribute__((cdecl)) i686_EnableInterrupts();
 uint8_t __attribute__((cdecl)) i686_DisableInterrupts();
 

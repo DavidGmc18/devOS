334f538d158e77fc376e46bd6c8c1e7176b5dcbb
Davidak <davidgames.youtub@gmail.com>
2026-02-02 14:13:46 +0100

    IO

--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -64,24 +64,6 @@
 
 %endmacro
 
-
-global x86_outb
-x86_outb:
-    [bits 32]
-    mov dx, [esp + 4]
-    mov al, [esp + 8]
-    out dx, al
-    ret
-
-global x86_inb
-x86_inb:
-    [bits 32]
-    mov dx, [esp + 4]
-    xor eax, eax
-    in al, dx
-    ret
-
-
 global x86_Disk_GetDriveParams
 x86_Disk_GetDriveParams:
     [bits 32]

60659b243b5a3d14693cd172e176518e5f2185da
Davidak <davidgames.youtub@gmail.com>
2026-03-26 21:14:10 +0100

    string.h

---
+++ lib/string.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdint.h>
+
+#define FILL_MULTIPLIER ((uintptr_t)(-1ULL / 0xFF))
+
+void* memset(void* ptr, int value, size_t num) {
+    uintptr_t fill = (uint8_t)value * FILL_MULTIPLIER;
+    size_t words = num / sizeof(uintptr_t);
+
+    uintptr_t* native_ptr = ptr;
+    for (size_t i = 0; i < words; i++)
+        native_ptr[i] = fill;
+
+    uint8_t* byte_ptr = ptr;
+    for (size_t i = words * sizeof(uintptr_t); i < num; i++)
+        byte_ptr[i] = value;
+
+    return ptr;
+}

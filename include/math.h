a65b7ec4c43d09ae991d39e63f0cb7be7fe3047e
Davidak <davidgames.youtub@gmail.com>
2026-04-07 14:04:48 +0200

    math.h

---
+++ include/math.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define ROUND_DOWN(a, b) ({         \
+    __typeof__(a) _a = (a);         \
+    __typeof__(b) _b = (b);         \
+    (_a / _b) * _b;                 \
+})
+
+#define ROUND_UP(a, b) ({           \
+    __typeof__(a) _a = (a);         \
+    __typeof__(b) _b = (b);         \
+    ((_a + _b - 1) / _b) * _b;      \
+})
